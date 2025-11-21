ee404724b95952cf49e8113a8ad29c70e2cafce1
Asim Kamal <asimkamalk@gmail.com>
2025-11-21 09:13:26 +0500

    storing components in components file

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,38 +1,10 @@
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcepts.jsx";
 import arrayOfObj from "./data.js";
 
-import componentsImg from "./assets/react-core-concepts.png";
 // import reactCore from "./assets/react-core-concepts.png";
 // import jsxImg from "./assets/jsx-ui.png";
 // import stateImg from "./assets/state-mgmt.png";
-const reactDescription = ["Fundamentals", "Crucial", "Core"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const desc = reactDescription[getRandomInt(2)];
-  return (
-    <header>
-      <img src={componentsImg} alt="Stylized atom" />
-      <h1>React Fundamentals</h1>
-      <p>
-        {desc} React concepts you will need for almost any app you are going to
-        build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ img, title, desc }) {
-  return (
-    <li>
-      <img src={img} alt={title} />
-      <h3>{title}</h3>
-      <p>{desc}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
@@ -48,31 +20,17 @@ function App() {
             title={arrayOfObj[0].title}
             desc={arrayOfObj[0].desc}
           />
-          <CoreConcept
-            img={arrayOfObj[1].img}
-            title={arrayOfObj[1].title}
-            desc={arrayOfObj[1].desc}
-          />
+        </ul> */}
 
-          <CoreConcept
-            img={arrayOfObj[2].img}
-            title={arrayOfObj[2].title}
-            desc={arrayOfObj[2].desc}
-          />
+        {/* <ul>
+          <CoreConcept {...arrayOfObj[1]} />
         </ul> */}
 
         <ul>
-          <CoreConcept {...arrayOfObj[0]} />
-          <CoreConcept {...arrayOfObj[1]} />
-          <CoreConcept {...arrayOfObj[2]} />
-          <CoreConcept {...arrayOfObj[3]} />
-        </ul>
-
-        {/* <ul>
           {arrayOfObj.map((item) => (
             <CoreConcept key={item.title} {...item} />
           ))}
-        </ul> */}
+        </ul>
       </section>
     </div>
   );
---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import componentsImg from "../assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamentals", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+function Header() {
+  const desc = reactDescription[getRandomInt(2)];
+  return (
+    <header>
+      <img src={componentsImg} alt="Stylized atom" />
+      <h1>React Fundamentals</h1>
+      <p>
+        {desc} React concepts you will need for almost any app you are going to
+        build!
+      </p>
+    </header>
+  );
+}
+
+export default Header;
